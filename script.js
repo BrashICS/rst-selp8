ad278c1f0823e6d45bb9cee42bd1dbdf4ea341cd
Selena <selena.pang@stu.ocsb.ca>
2024-06-06 09:51:18 -0400

    gitting the hub

--- script.js
+++ script.js
@@ -8,11 +8,41 @@
 
 'use strict';
 //class for the targets
+let canvas = document.getElementById("myCanvas");
 class Target{
+  //set properties such as: hp, visible (bool), size,
+  //size will be randomly generated later
+  #hp = 50;
+  #size;
+  #visible = true;
+  #posx;
+  #posy;
 
+  constructor(size, posx,posy){
+    this.#size=size;
+    this.#posx=posx;
+    this.#posy=posy;
+  }
+
+  get visible(){
+    return this.#visible;
+  }
+
+  set size(yaySize){
+    this.#size = yaySize;
+    return this.#size;
+  }
+
+  set hp(dmg){
+    if(hp-dmg<=0){
+      this.#visible=false;
+    }
+    this.#hp-=dmg;
+    return this.#hp;
+  }
 }
 
-//dragging and stuff functions so nibbles can move position
+//dragging and stuff functions so nibbles can move position - DONE
 function drag(event){
   event.preventDefault();
   let data = event.dataTransfer.getData("image");
@@ -21,7 +51,6 @@ function drag(event){
 
 function allow(event){
   event.preventDefault();
-
 }
 
 function dragStart(event){
@@ -34,5 +63,7 @@ function fire(event){
 }
 
 //collision function
+function detectCol(){
 
+}
 
