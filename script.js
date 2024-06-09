c29732a7b600134fe7ab64387d1ae9e3439874c9
Selena <selena.pang@stu.ocsb.ca>
2024-06-09 02:22:38 -0400

    pewpew workngYAY FINALLY

--- script.js
+++ script.js
@@ -52,7 +52,33 @@ class Player{
     }
   }
 }
+
+class Pewpew{
+  constructor({position, going}){
+    this.position = position;
+    this.going = going;
+    this.radius=3; //going to be circular so radius by default
+  }
+
+  //method time - similar to player but not exactly same so inheritance no thank you
+  draw(){
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI *2); //we use it to make full arc so a circle
+    ctx.fillStyle = "pink";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update(){
+    this.draw();
+    this.position.x+=this.going.x;
+    this.position.y+=this.going.y;
+  }
+
+}
 const nibbles = new Player();
+//to make array of pewpews, so multiple
+const pewpews = [];
 //object keys to detect keys up and down
 //set false by default
 const keys = {
@@ -65,20 +91,34 @@ const keys = {
   space: {
     pressed:false
   }
-}
-//nibbles.draw();
+};
 
-//listen for A and D pressed so nibbles can move!!!!
+
+//listen for A and D pressed so nibbles can move and when not pressed, nibbles stops moving
 addEventListener("keydown", move);
 addEventListener("keyup", stop);
 
-//i make function so it continueously shows up - recursion... no stopping tho.. keep calling.. nibbles should always be there
-//i learn new thing:request for frame to animate, function called ...nibbles always being drawn
+//i make function so things continueously shows up - keep calling..
 function plsshowup(){
+  //i learn new thing:request for frame to animate, function called ..
   requestAnimationFrame(plsshowup);
+  //bg
   ctx.fillStyle = 'black';
   ctx.fillRect(0,0,myCanvas.width,myCanvas.height);
   nibbles.update();
+  //new function learnt: foreach: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_foreach
+  //func for EACH element in array
+  pewpews.forEach((pewpew, index) => { //have to use arrow function so index provided from forEach can be used to remove pewpews
+    //remove pewpew from array once off screen
+    if(pewpew.position.y+pewpew.radius<=0){
+      pewpews.splice(index, 1);
+    }
+    else{
+      pewpew.update();
+    }
+    pewpew.update();
+
+  });
 
   //only animate when key is being pressed - determined by object
   //and not going off the damn screen
@@ -99,16 +139,27 @@ plsshowup();
 function move(event){
   //console.log(event.key);
   if(event.key=='a'){
-    console.log("left");
+   // console.log("left");
     keys.a.pressed=true;
   }
   if(event.key=='d'){
-    console.log("right");
+    //console.log("right");
     keys.d.pressed=true;
   }
   if(event.key==' '){
-    console.log("space");
-    keys.space.pressed=true;
+   // console.log("space");
+    pewpews.push( //we are pushing a new object into the array created earlier, causing it to go up to screen
+    new Pewpew({
+      position: {
+        //have the thingies shoot above nibbles head
+        x:nibbles.position.x + nibbles.width /2,
+        y:nibbles.position.y
+      },
+      going: {
+        x:0, y:-8
+      }
+    })
+  )
   }
 }
 
@@ -128,6 +179,7 @@ function stop(event){
     keys.space.pressed=false;
   }
 }
+
 
 
 
